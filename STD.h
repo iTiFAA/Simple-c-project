81bf6d40a8b507829f4a9d5dcc547f34bf15977d
TiFA <71542863+iTiFAA@users.noreply.github.com>
2024-08-12 19:22:06 +0300

    Add files via upload

---
+++ STD.h
@@ -0,0 +1,13 @@
+#ifndef EMBEDDED_SYS_STD_H
+#define EMBEDDED_SYS_STD_H
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int int16;
+typedef unsigned short int uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef enum e{
+    false,
+    true
+}bool;
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "c project/SDBAPP.h"
+int main(){
+    SDB_APP();
+return 0;
+}
